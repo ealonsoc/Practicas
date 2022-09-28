1b915df98cc9e70aedb715a526de9b0b2fab5ca8
ealonsoc <ealonsoc@gomsa.com>
2022-09-28 14:07:56 +0000

    [ADD] Basic Module

---
+++ odoo_espacial/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Odoo Espacial',
+    'summary': """Espacial app para viajes estelares""",
+    'description':"""
+        Modulo para viajes estelares.
+        -Naves
+        -Viajes
+        -Tripulacion
+    """,
+    
+    'author':'Odoo',
+    
+    'website': "https://www.odoo.com",
+    
+    'category':'Training',
+    'version':'0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+        
+    ],
+    'license':'OPL-1',
+    
+    'demo':[
+        
+    ],
+    
+}
